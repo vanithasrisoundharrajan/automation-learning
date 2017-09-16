f3f3905342f6d735823569f7007fd2467fdb6a62
Design Experts <Design Experts@DesignExperts>
2017-09-16 14:53:42 +0530

    new download

--- Selenium02/src/DownloadFile.java
+++ Selenium02/src/DownloadFile.java
@@ -11,11 +11,11 @@ public class DownloadFile {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","D:/eclipse/selenium/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
+	
 		driver.get("http://www.java2s.com/Code/Jar/o/Downloadojdbc142jar.htm");
 		driver.findElement(By.linkText("ojdbc/ojdbc-1.4.2.jar.zip( 1,454 k)")).click();
 		
-		
+	
 	}
 
 }
