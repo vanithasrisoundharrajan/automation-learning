1274941c863e140eeeff8f5658b489840d9ebf2b
Design Experts <Design Experts@DesignExperts>
2017-09-16 14:26:31 +0530

    New DownloadFile

--- Selenium02/src/DownloadFile.java
+++ Selenium02/src/DownloadFile.java
@@ -1,4 +1,6 @@
 import java.awt.AWTException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,11 +11,11 @@ public class DownloadFile {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","D:/eclipse/selenium/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.google.co.in");
 		driver.manage().window().maximize();
+		driver.get("http://www.java2s.com/Code/Jar/o/Downloadojdbc142jar.htm");
+		driver.findElement(By.linkText("ojdbc/ojdbc-1.4.2.jar.zip( 1,454 k)")).click();
 		
-	    
-
+		
 	}
 
 }
