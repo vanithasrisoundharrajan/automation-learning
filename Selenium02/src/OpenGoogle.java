a7af3bd77c5d64423fe52c614579f2c9d5526c1e
Design Experts <Design Experts@DesignExperts>
2017-09-16 15:50:07 +0530

    TestNg

---
+++ Selenium02/src/OpenGoogle.java
@@ -0,0 +1,24 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenGoogle {
+	@Test(priority=1)
+	public  void openGoogle(){
+		System.setProperty("webdriver.chrome.driver","D:/eclipse/selenium/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to("http://www.google.co.in");
+		driver.manage().window().maximize();
+		
+		
+	}
+	@ Test(priority=2)
+	public void openBing(){
+		System.setProperty("webdriver.chrome.driver","D:/eclipse/selenium/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to("http://www.bing.com");
+		driver.manage().window().maximize();
+	}
+
+}
