6c23554456eba038f20cf00fdc9c572649271867
Design Experts <Design Experts@DesignExperts>
2017-09-17 13:09:40 +0530

    Timecalculation

---
+++ Selenium02/src/TestNG/TimeCalculation.java
@@ -0,0 +1,43 @@
+package TestNG;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TimeCalculation {
+	WebDriver driver;
+	long starttime;
+	long endtime;
+	long totaltime;
+	@BeforeSuite
+	public void openBrowser (){
+	starttime=System.currentTimeMillis();
+      System.setProperty("webdriver.chrome.driver","D:/eclipse/selenium/chromedriver.exe");
+      driver=new ChromeDriver();
+      driver.manage().window().maximize(); 
+	}
+	@Test
+	public void openGoogle (){
+		driver.get("http://www.google.co.in");
+	}
+	
+	@Test
+	public void openBing (){
+		driver.get("http://www.bing.co.in");
+	}
+	
+	@AfterSuite
+	public void close(){
+		driver.quit();
+	
+	
+	
+	 endtime=System.currentTimeMillis();
+	 totaltime=endtime-starttime;
+	System.out.println(totaltime/1000.0 + "seconds"); 
+
+}
+}
+
