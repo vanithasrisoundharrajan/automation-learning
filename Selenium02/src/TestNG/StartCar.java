dbf7434a1fb783241c819d4c25f644331fcd97d7
Design Experts <Design Experts@DesignExperts>
2017-09-17 13:07:18 +0530

    TimeCalculation

---
+++ Selenium02/src/TestNG/StartCar.java
@@ -0,0 +1,31 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class StartCar {
+	@Test
+    public void startEngine() {
+    	System.out.println("EngineStart");
+		
+	}
+	@Test
+    public void putFirstGear() {
+    	
+    	System.out.println("putFirstGear");
+		
+	}
+	@Test
+ public void putSecondGear() {
+    	
+    	System.out.println("putSecondGear");
+		
+	}
+	@Test
+ public void putThirdGear() {
+ 	
+ 	System.out.println("putThirdGear");
+		
+	}
+
+    
+}
